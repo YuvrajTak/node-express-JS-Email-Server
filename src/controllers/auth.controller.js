727e9a6dd8f75a92eb84b842e94177e76a4aa0fe
yuvraj.tak <yuvraj.tak@squarebits.com>
2024-11-15 19:19:51 +0530

    feat: mail server route and method added.

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,86 @@
+const httpStatus = require('http-status');
+const nodemailer = require('nodemailer');
+const catchAsync = require('../utils/catchAsync');
+const config = require('../config/config');
+const { authService, userService, tokenService, emailService } = require('../services');
+
+const register = catchAsync(async (req, res) => {
+  const user = await userService.createUser(req.body);
+  const tokens = await tokenService.generateAuthTokens(user);
+  res.status(httpStatus.CREATED).send({ user, tokens });
+});
+
+const login = catchAsync(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await authService.loginUserWithEmailAndPassword(email, password);
+  const tokens = await tokenService.generateAuthTokens(user);
+  res.send({ user, tokens });
+});
+
+const logout = catchAsync(async (req, res) => {
+  await authService.logout(req.body.refreshToken);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+const refreshTokens = catchAsync(async (req, res) => {
+  const tokens = await authService.refreshAuth(req.body.refreshToken);
+  res.send({ ...tokens });
+});
+
+const forgotPassword = catchAsync(async (req, res) => {
+  const resetPasswordToken = await tokenService.generateResetPasswordToken(req.body.email);
+  await emailService.sendResetPasswordEmail(req.body.email, resetPasswordToken);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+const resetPassword = catchAsync(async (req, res) => {
+  await authService.resetPassword(req.query.token, req.body.password);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+const sendVerificationEmail = catchAsync(async (req, res) => {
+  const verifyEmailToken = await tokenService.generateVerifyEmailToken(req.user);
+  await emailService.sendVerificationEmail(req.user.email, verifyEmailToken);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+const verifyEmail = catchAsync(async (req, res) => {
+  await authService.verifyEmail(req.query.token);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+const transport = nodemailer.createTransport(config.email.smtp);
+
+const sendEmail = catchAsync(async (req, res) => {
+   console.log("first-----------------", process.env.SMTP_PASSWORD)
+
+  const { to, subject, text } = req.body;
+
+  const mailOptions = {
+    from:  process.env.SMTP_USERNAME,
+    to,
+    subject,
+    text,
+  };
+ console.log("mailOptions",mailOptions)
+
+  try {
+    await transport.sendMail(mailOptions);
+    res.status(200).send('Email sent successfully!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Failed to send email.');
+  }
+});
+
+module.exports = {
+  register,
+  login,
+  logout,
+  refreshTokens,
+  forgotPassword,
+  resetPassword,
+  sendVerificationEmail,
+  verifyEmail,
+  sendEmail
+};
